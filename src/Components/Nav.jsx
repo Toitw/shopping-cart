cce171ce3b9fd8b6ffaa4c2485f70426d798a5b1
juroga_new <juanjrgast@gmail.com>
2023-12-09 18:26:09 +0100

    Update Nav component styles

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import Cart from './Cart';
+import styles from './Nav.module.css';
 
 const Nav = () => {
     return (
         <nav>
-            <div className="nav-links">
-                <Link to="/" className="nav-link">Home</Link>
+            <div className={styles.navContainer}>
+                <Link to="/" className={styles.navLink}>Home</Link>
+                <Cart className={styles.navLink}/>
             </div>
-            <Cart />
         </nav>
     );
 };
