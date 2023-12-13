5a374741bd57a0768f8929c87e8ebfdd99f4c08c
juroga_new <juanjrgast@gmail.com>
2023-12-13 19:39:50 +0100

    Add checkout functionality and order confirmation modal

--- src/Components/CheckoutPage.jsx
+++ src/Components/CheckoutPage.jsx
@@ -1,20 +1,44 @@
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import { useState } from "react";
 
 const CheckoutPage = () => {
-  const {cartItems} = useContext(CartContext);
+  const {cartItems, setCartItems} = useContext(CartContext);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCheckout = () => {
+    setCartItems([]);
+    setShowModal(true);
+  };
 
   return (
     <div>
-      <h1>Checkout Resume</h1>
-      {cartItems.map((item) => (
-        <div key={item.id}>
-          <h3>{item.title}</h3>
-          <p>{item.price}€</p>
-          <p>Quantity: {item.qty}</p>
-          {/* <img src={item.image} alt={item.title} /> */}
+      {cartItems.length === 0 ? (
+        <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+          No items found
         </div>
-      ))}
+      ) : (
+        cartItems.map((item) => (
+          <div key={item.id}>
+            {/* <img src={item.image} alt={item.title} /> */}
+            <h3>{item.title}</h3>
+            <p>{item.price}€</p>
+            <p>Quantity: {item.qty}</p>
+          </div>
+        ))
+      )}
+  
+      {cartItems.length > 0 && (
+        <button onClick={handleCheckout}>Checkout</button>
+      )}
+  
+      {showModal && (
+        <div>
+          Your order is on the way!
+          <button onClick={() => setShowModal(false)}>Close</button>
+        </div>
+      )}
     </div>
   );
 };
